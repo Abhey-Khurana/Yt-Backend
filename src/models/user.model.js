9da996f263f9bac8c368ce88eae00bedd35316b3
Abhey Khurana <abheykhurana73@gmail.com>
2024-05-18 19:15:35 +0530

    Added User and Video Model

---
+++ src/models/user.model.js
@@ -0,0 +1,91 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import mongoose, { Schema } from "mongoose";
+
+
+const schemaObj = {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //Cloudinary URL
+        required: true
+    },
+    coverImage: {
+        type: String //Cloudinary URL
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    },
+    watchHistory: {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }
+}
+
+const UserSchema = new Schema(schemaObj, { timestamps: true });
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+UserSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+UserSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        id: this.id
+    },
+        process.env.REFRESH_ACCESS_TOKEN,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+const User = mongoose.Model('User', UserSchema);
+
+
+
+export { User }
---
+++ src/models/video.model.js
@@ -0,0 +1,50 @@
+import generateModel from "./generateModel";
+import mongoose, { Schema } from "mongoose";
+import aggrigatePaginate from 'mongoose-aggregate-paginate-v2';
+
+const schemaObj = {
+    videoFile: {
+        type: String, //Cloudinary 
+        required: true
+    },
+    thumbnail: {
+        type: String, //Cloudinary 
+        required: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}
+
+const VideoSchema=new Schema(schemaObj,{timestamps:true});
+
+VideoSchema.plugin(aggrigatePaginate);
+
+const Video = mongoose.Model('Video',VideoSchema);
+
+export { Video }
